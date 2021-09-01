ccdf3f3a73b080754c9524d7abc75ffa384e65fd
RandyLu0 <lurandy8@gmail.com>
2021-09-01 15:36:04 -0400

    added leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,25 @@
+//File name: /home/randy/135-rec/fall-2021-13500-lab-01-RandyLu0/leap.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "Enter year: ";
+    cin >> year;
+    if(year % 4 != 0){
+        cout << "\n\nCommon year\n";
+    }else{
+        if(year % 100 != 0){
+            cout << "\n\nLeap year\n";
+        }else{
+            if(year % 400 != 0){
+                cout << "\n\nCommon year\n";
+            }else{
+                cout << "\n\nLeap year\n";
+            }
+        
+            }
+        }
+    return 0;
+}
