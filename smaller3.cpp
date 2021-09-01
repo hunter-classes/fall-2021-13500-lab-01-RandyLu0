b844442eddca9fbf3a6f81b48eb5bfee73c8ac7a
RandyLu0 <lurandy8@gmail.com>
2021-09-01 15:17:52 -0400

    added smaller3.cpp

---
+++ smaller3.cpp
@@ -0,0 +1,28 @@
+//File name: /home/randy/135-rec/fall-2021-13500-lab-01-RandyLu0/smaller3.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z,s;
+
+    cout << "\nEnter the first number: ";
+    cin >> x;
+    cout << "\nEnter the second number: ";
+    cin >> y;
+    cout << "\nEnter the third number: ";
+    cin >> z;
+    if (x < y and x < z){
+        s = x;
+    }else{
+    if (y < x and y < z){
+        s = y;
+    }else{
+        s = z;
+    }
+    }
+    cout << "\n\nThe smallest number is " << s;
+    
+
+    return 0;
+}
