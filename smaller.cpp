642233ae0e770d73606ec3686c843c48449fb05a
RandyLu0 <lurandy8@gmail.com>
2021-09-01 15:14:55 -0400

    added smaller.cpp

---
+++ smaller.cpp
@@ -0,0 +1,20 @@
+//File name: /home/randy/135-rec/fall-2021-13500-lab-01-RandyLu0/smaller.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,m,s;
+
+    cout << "\nEnter the first number: ";
+    cin >> n;
+    cout << "\nEnter the second number: ";
+    cin >> m;
+    if(n < m){
+        s = n;
+    }else{
+        s = m;
+    }
+    cout << "\n\nThe smallest number is " << s;
+    return 0;
+}
