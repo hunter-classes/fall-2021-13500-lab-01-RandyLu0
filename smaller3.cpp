89a40c9b522aeb0dbd88307abc72e8195168fe5f
RandyLu0 <lurandy8@gmail.com>
2021-09-02 15:30:20 -0400

    updated file

--- smaller3.cpp
+++ smaller3.cpp
@@ -2,26 +2,24 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     int x,y,z,s;
-
     cout << "\nEnter the first number: ";
     cin >> x;
     cout << "\nEnter the second number: ";
     cin >> y;
     cout << "\nEnter the third number: ";
     cin >> z;
-    if (x < y and x < z){
+    if (x <= y and x <= z){
         s = x;
     }else{
-    if (y < x and y < z){
+    if (y <= x and y <= z){
         s = y;
     }else{
         s = z;
     }
     }
-    cout << "\n\nThe smallest number is " << s;
+    cout << "\n\nThe smallest number is " << s << endl;
     
 
     return 0;
