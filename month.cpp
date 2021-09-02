470bd71c9957acf7d027f76a5c370d35105a2cb2
RandyLu0 <lurandy8@gmail.com>
2021-09-02 16:44:23 -0400

    added month.cpp

---
+++ month.cpp
@@ -0,0 +1,45 @@
+//File name: /home/randy/135-rec/fall-2021-13500-lab-01-RandyLu0/month.cpp
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main(){
+
+    int year, month;
+
+    cout << "Enter the year: ";
+    cin >> year;
+    cout << "\nEnter the month: ";
+    cin >> month;
+
+    if(month < 8 and month % 2 == 1){
+        cout << "\nThere are 31 days\n";
+    }else{
+		if(month >= 8 and month % 2 == 0){
+			cout << "\nThere are 31 days\n";		
+		}else{
+			if(month < 8 and month % 2 == 0 and month != 2){
+				cout << "\nThere are 30 days\n";
+			}else{
+				if(month > 8 and month % 2 == 1){
+					cout << "\nThere are 30 days\n";	
+				}else{
+					if(month == 2 and year % 4 != 0){
+						cout << "\nThere are 28 days\n";
+					}else{
+						if(year % 400 == 0){
+							cout << "\nThere are 29 days\n";
+						}else{
+							if(year % 100 == 0){
+								cout << "\nThere are 28 days\n";
+							}else{
+								cout << "\nThere are 29 days\n";
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
