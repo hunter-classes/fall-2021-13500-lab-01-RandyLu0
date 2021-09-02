67707a5979e26ba2e7412637465170c8ce99c72e
RandyLu0 <lurandy8@gmail.com>
2021-09-02 16:43:39 -0400

    updated file

--- smaller.cpp
+++ smaller.cpp
@@ -1,20 +1,24 @@
 //File name: /home/randy/135-rec/fall-2021-13500-lab-01-RandyLu0/smaller.cpp
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::cin;
 
 int main(){
 
     int n,m,s;
 
-    cout << "\nEnter the first number: ";
+    cout << "Enter the first number: ";
     cin >> n;
     cout << "\nEnter the second number: ";
     cin >> m;
+    
     if(n <= m){
         s = n;
     }else{
 		s = m;
     }
-	cout << "\n\nThe smallest number is " << s << endl;
+
+	cout << "\n\nThe smallest number is " << s << "\n";
+    
     return 0;
 }
