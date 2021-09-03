908316a12f5fd1f9344dd438bceab11946404023
RandyLu0 <lurandy8@gmail.com>
2021-09-03 19:59:26 -0400

    updated file

--- leap.cpp
+++ leap.cpp
@@ -1,27 +1,19 @@
-//File name: /home/randy/135-rec/fall-2021-13500-lab-01-RandyLu0/leap.cpp
 #include <iostream>
 using std::cout;
 using std::cin;
 
 int main(){
-
-    int year;
-
-    cout << "Enter year: ";
-    cin >> year;
-    
-    if(year % 4 != 0){
-        cout << "\n\nCommon year\n";
-    }else{
-        if(year % 100 != 0){
-            cout << "\n\nLeap year\n";
-        }else{
-            if(year % 400 != 0){
-                cout << "\n\nCommon year\n";
-            }else{
-                cout << "\n\nLeap year\n";
-            }    
-        }
-    }
-    return 0;
+	cout << "Enter a year: ";
+	int y;
+	cin >> y;
+	if(y % 4 == 0 and y % 100 != 0){
+		cout << "\n\nLeap year\n";
+	}else{
+		if(y % 400 == 0){
+			cout << "\n\nLeap year\n";
+		}else{
+			cout << "\n\nCommon year\n";
+		}
+	}
+	return 0;
 }
